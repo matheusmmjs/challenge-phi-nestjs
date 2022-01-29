43937351974fbe3437418d54909d43f02718b07e
Matheus Santos <matheusmatmac@hotmail.com>
2022-01-29 14:21:52 -0300

    feat: Adjust import dotenv config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import 'dotenv/config';
 
 @Module({
   imports: [
