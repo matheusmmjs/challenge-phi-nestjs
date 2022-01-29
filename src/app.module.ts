d2d1f889528fac68083fc01d97fb94b5899a5886
Matheus Santos <matheusmatmac@hotmail.com>
2022-01-29 13:58:03 -0300

    feat: :sparkles: Added dotenv and mongoose, refactor project and configured mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@cluster0.jctsk.mongodb.net/test`,
+    ),
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
